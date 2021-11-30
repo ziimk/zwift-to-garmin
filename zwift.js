a0e03f384eaaf23c6cd4771bdedf05325d303a8c
Siim Kibus <siim.kibus@pipedrive.com>
2021-11-30 23:51:46 +0200

    Update zwift login steps

--- zwift.js
+++ zwift.js
@@ -13,9 +13,9 @@ async function downloadActivity(page) {
 }
 
 async function logIn(page) {
-	await page.fill('#username', zwift.username);
-	await page.fill('#password', zwift.password);
-	await page.click('#submit-button');
+	await page.fill('#email-input', zwift.username);
+	await page.fill('#password-input', zwift.password);
+	await page.click('button:near(.SignInForm-module__actions--eEQsl)', { timeout: 90000 });
 }
 
 async function navigateToLatestActivity(page) {
@@ -23,9 +23,9 @@ async function navigateToLatestActivity(page) {
 }
 
 async function navigateToLogin(page) {
-	await page.goto('https://zwift.com/eu');
+	await page.goto('https://www.zwift.com/sign-in?redirect_uri=https%3A%2F%2Fwww.zwift.com%2Ffeed%3Fauth_redirect%3Dtrue');
 	await page.waitForTimeout(2500);
-	await page.click('#znv-header-login-button');
+	await page.click('#truste-consent-button');
 }
 
 module.exports = {
